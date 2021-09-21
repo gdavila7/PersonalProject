fcb9ceb7b4a790d91b264ce79950b426636e0474
Gustavo Davila <gustavodavila@Admins-iMac.domain.name>
2021-09-21 10:42:37 -0500

    add matchbox
    matchboc help to create prettier conf, ESlint and git hooks

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 
-const app = express(); 
+const app = express();
 
 app.get('/', (req, res) => {
-    res.json({
-        "message": "Hi world tech"
-    })
-})
+  res.json({
+    message: 'Hi world tech',
+  });
+});
 
 app.listen(7000, () => console.log('server on port 7000'));
