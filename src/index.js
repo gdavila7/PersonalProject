a8e7b2b1a1b1c9d654f53b783e9f9a75ac2554db
Gustavo Alexander Davila <gdavila@condorlabs.io>
2021-09-21 11:15:40 -0500

    add logger helper

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+const logger = require('@condor-labs/logger');
 
 const app = express();
 
 app.get('/', (req, res) => {
+  logger.information('/ is working');
   res.json({
     message: 'Hi world tech',
   });
